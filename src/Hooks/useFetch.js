685f9b2271211c2c7e345d94e66fcbb606d83cb4
ShynRdi <rdishyn@gmail.com>
2023-07-11 11:36:50 +0330

    adding logic of readily

---
+++ src/Hooks/useFetch.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from "react";
+import { connect, useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from "../redux/users/usersAction";
+import { fetchBooks } from "../redux/bookList/bookListAction";
+export default function useFetch(action, entity) {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(action());
+  }, []);
+
+  const storeData = useSelector((state) => ({
+    data: state[entity].list,
+    loading: state[entity].loading,
+    error: state[entity].error,
+  }));
+
+  //   const { data, loading, error } = storeData;
+
+  return storeData;
+}
